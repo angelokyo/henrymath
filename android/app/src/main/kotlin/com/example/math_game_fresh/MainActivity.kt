7a3f39fda98785b477e7ac5b3ff51460f0b8b9e3
Junxing JX1 Lan <lanjx1@Lenovo.com>
2025-06-06 23:04:29 +0800

    Final clean deploy with correct base href

--- android/app/src/main/kotlin/com/example/math_game_fresh/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.math_game_fresh
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "33a290526d8ff6ebb7bdf6a0c64594be
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "96453627aa0b55e372a7268720f8710a",
+"flutter_bootstrap.js": "60a102a39cbea16b5535162d15faaa11",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
